9844e87df217daacae3d6594d4c6e12cc9fccf5f
Zeezbaba <okelejitimi93@gmail.com>
2025-07-14 22:45:14 +0100

    Task 0: Basic IP Logging Middleware

---
+++ alx_backend_security/ip_tracking/middleware.py
@@ -0,0 +1,25 @@
+from django.utils.timezone import now
+from .models import RequestLog
+
+class IPTracker:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        ip = self.get_client_ip(request)
+        path = request.path
+        timestamp = now()
+
+        # save to database
+        RequestLog.objects.create(ip_address=ip, timestamp=timestamp, path=path)
+
+        return self.get_response(request)
+
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(",")[0]
+
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
